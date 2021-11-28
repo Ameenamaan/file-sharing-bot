7b7f08310b34e2388072083065e03360d3716f91
Ameenamaan <88217467+Ameenamaan@users.noreply.github.com>
2021-11-28 11:45:13 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/COLD_ONEZ'>Cᴏʟᴅ々Oɴᴇᴢ࿐</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/COLD-ONEZ/File-SHare-Bot'>Click Here</a>\n○ Cʜᴀɴɴᴇʟ : @Mo_Tech_YT\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @Mo_Tech_Group</b>",
+            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/Ameen_Ameen_12'>ᵐʳ𝐔𝐧𝐤𝐧𝐨𝐰𝐧🇮🇳༒</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/Ameenamaan/file-sharing-bot'>Click Here</a>\n○ Cʜᴀɴɴᴇʟ : @HDmovieDp\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @HDmoviePp</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
